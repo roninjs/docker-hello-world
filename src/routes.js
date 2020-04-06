3435c3b329dd4119aa85e143d836decf913d86a2
peter <peter.mckee@gmail.com>
2020-04-06 15:27:11 -0500

    change main route to list all paths

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express 	= require( 'express' )
 const appRouter = express.Router()
 
-appRouter.get( '/', function( req, res ) {
+appRouter.get( '/*', function( req, res ) {
   res.send( '<h1>Hello, Docker</h1>' )
 })
 

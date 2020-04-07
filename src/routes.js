b3262def0a7801d1156fc594d4d77c7deba36a50
peter <peter.mckee@gmail.com>
2020-04-07 17:06:09 -0500

    Update Branch

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ const express 	= require( 'express' )
 const appRouter = express.Router()
 
 appRouter.get( '/*', function( req, res ) {
-  res.send( '<h1>Hello from Autobuilds</h1>' )
+  res.send( '<h1>Hello Autobuilds</h1>' )
 })
 
 module.exports = appRouter
